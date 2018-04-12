aed1d19bd90d7117243552a7211da838163e3228
ibrahim Eser <kernhal@gmail.com>
2018-04-12 12:24:35 +0300

    file re-formatted using clang-format

--- src/aldrone_teleop.h
+++ src/aldrone_teleop.h
@@ -1,6 +1,7 @@
 
 /*
- * This program is aiming to provide an interface for SONY PS3-like controller for controlling ArDrone in ROS.
+ * This program is aiming to provide an interface for SONY PS3-like controller
+ * for controlling ArDrone in ROS.
  *
  * mostly inpired by turtlesim/joy tutorial.
  * Author: ibrahim Eser <ibrahimeser@gmx.com.tr>
@@ -9,54 +10,52 @@
 #ifndef __ALDRONE_TELEOP_H
 #define __ALDRONE_TELEOP_H
 
-
+#include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
-#include <geometry_msgs/Twist.h>
 #include <std_msgs/Empty.h>
 #include <std_srvs/Empty.h>
 
 #define SPIN_RATE 10
 
-class AldroneTeleop
-{
-    public:
-        AldroneTeleop();
+class AldroneTeleop {
+ public:
+  AldroneTeleop();
 
-    private:
-        bool got_first_joy_msg;
-        bool toggle_pressed_in_last_msg;
-        bool cam_toggle_pressed_in_last_msg;
+ private:
+  bool got_first_joy_msg;
+  bool toggle_pressed_in_last_msg;
+  bool cam_toggle_pressed_in_last_msg;
 
-        bool is_flying;
-        bool dead_man_pressed;
+  bool is_flying;
+  bool dead_man_pressed;
 
-        int axis_x;
-        int axis_y;
-        int axis_z;
-        int axis_yaw;
+  int axis_x;
+  int axis_y;
+  int axis_z;
+  int axis_yaw;
 
-        double scale_x;
-        double scale_y;
-        double scale_z;
-        double scale_yaw;
+  double scale_x;
+  double scale_y;
+  double scale_z;
+  double scale_yaw;
 
-        int btn_dead_man;
-        int btn_emergency;
-        int btn_cam_toggle;
+  int btn_dead_man;
+  int btn_emergency;
+  int btn_cam_toggle;
 
-        geometry_msgs::Twist twist;
-        std_srvs::Empty srv_empty;
+  geometry_msgs::Twist twist;
+  std_srvs::Empty srv_empty;
 
-        ros::NodeHandle nh_;
-        ros::Publisher pub_vel;
-        ros::Publisher pub_takeoff;
-        ros::Publisher pub_land;
-        ros::Publisher pub_toggle_state;
-        ros::Subscriber sub_joy;
-        ros::ServiceClient srv_cl_cam;
+  ros::NodeHandle nh_;
+  ros::Publisher pub_vel;
+  ros::Publisher pub_takeoff;
+  ros::Publisher pub_land;
+  ros::Publisher pub_toggle_state;
+  ros::Subscriber sub_joy;
+  ros::ServiceClient srv_cl_cam;
 
-        void joyCallBack (const sensor_msgs::Joy::ConstPtr& joy);
+  void joyCallBack(const sensor_msgs::Joy::ConstPtr& joy);
 };
 
 #endif /* __ALDRONE_TELEOP_H */
